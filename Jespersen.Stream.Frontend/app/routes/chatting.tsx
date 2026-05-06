094f5bab56bc7465f12fd43c4e204ab3fee0be6a
jesperrichert <jesper.l.richert@gmail.com>
2026-05-06 22:21:40 +0200

    Added Ending and Chatting page.

--- Jespersen.Stream.Frontend/app/routes/chatting.tsx
+++ Jespersen.Stream.Frontend/app/routes/chatting.tsx
@@ -1,4 +1,5 @@
 export default function Chatting() {
   return <div>
+
   </div>;
 }
--- Jespersen.Stream.Frontend/app/routes/ending.tsx
+++ Jespersen.Stream.Frontend/app/routes/ending.tsx
@@ -1,6 +1,5 @@
 
 export default function Ending() {
-  return <div className={"bg-[url(/assets/ending/background.png)] min-h-screen"}>
-    <canvas width={500} height={500}></canvas>
+  return <div className={"bg-[url(/assets/ending/background.gif)] min-h-screen"}>
   </div>;
 }
